6aa7291107b15be468582c2e2dbf114afcf38be1
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-07 13:55:16 -0500

    init

---
+++ src/main/java/com/isai/demojasperreportsystembilling/app/DemoJasperReportSystemBillingApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demojasperreportsystembilling.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJasperReportSystemBillingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJasperReportSystemBillingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isai/demojasperreportsystembilling/app/DemoJasperReportSystemBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demojasperreportsystembilling.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJasperReportSystemBillingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
